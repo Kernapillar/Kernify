5469ccace6137e993ccc16cf9821fa00f34d2fb9
Alexander Kern <alexander.t.kern@gmail.com>
2022-08-18 14:23:38 -0700

    added social links

--- frontend/components/ui_blocks/header.jsx
+++ frontend/components/ui_blocks/header.jsx
@@ -4,7 +4,7 @@ import Dropdown from "../dropdown_components/user_dropdown";
 
 const header = () => (
     <div className="header-topbar">
-        {/* <Dropdown /> */}
+        
         <GreetingContainer />
     </div>
 )
--- frontend/components/ui_blocks/sidebar.jsx
+++ frontend/components/ui_blocks/sidebar.jsx
@@ -1,34 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
-const SideBar = () => (
-    <div className="sidebar">
-        <Link to="/"> <img src="https://kernify-seed.s3.us-west-1.amazonaws.com/kernify_logo.png" alt="Kernify Logo" />
-        </Link>
-        <Link to="/"> <button><span className="material-symbols-outlined">home</span>
-    &nbsp; Home</button></Link>
-        <br />
+const SideBar = () => ( 
+    <>
+        <div className="sidebar">
+            <nav>
+            <Link to="/"> <img src="https://kernify-seed.s3.us-west-1.amazonaws.com/kernify_logo.png" alt="Kernify Logo" />
+            </Link>
+            <Link to="/"> <button><span className="material-symbols-outlined">home</span>
+            &nbsp; Home</button></Link>
+            <br />
 
-        <Link to="/search"> <button><span className="material-symbols-outlined">search</span>
-    &nbsp; Search</button></Link>
-        <br />
+            <Link to="/search"> <button><span className="material-symbols-outlined">search</span>
+            &nbsp; Search</button></Link>
+            <br />
 
-        <Link to="/artists"> <button><span className="material-symbols-outlined">person</span>
-    &nbsp; Artists</button></Link>
-        <br />
+            <Link to="/artists"> <button><span className="material-symbols-outlined">person</span>
+            &nbsp; Artists</button></Link>
+            <br />
 
-        <Link to="/albums"> <button><span className="material-symbols-outlined">library_music</span>
-    &nbsp; Albums</button></Link>
-        <br />
+            <Link to="/albums"> <button><span className="material-symbols-outlined">library_music</span>
+            &nbsp; Albums</button></Link>
+            <br />
+            
+            <Link to="/playlists"> <button><span className="material-symbols-outlined">queue_music</span>
+            &nbsp; Playlists</button></Link>
+            
+            <br />
+            <Link to="/playlists/create"> <button><span className="material-symbols-outlined">add_box</span>
+            &nbsp; Create Playlist</button></Link>
+
+            </nav>
         
-        <Link to="/playlists"> <button><span className="material-symbols-outlined">queue_music</span>
-    &nbsp; Playlists</button></Link>
-        
-        <br />
-        <Link to="/playlists/create"> <button><span className="material-symbols-outlined">add_box</span>
-    &nbsp; Create Playlist</button></Link>
-    
-    </div>
+        <div className="social">
+            <a href="https://github.com/Kernapillar/Kernify" target="_blank"> 
+                <img className="social-link" src="https://kernify-seed.s3.us-west-1.amazonaws.com/github_white_logo.png" alt="github link" />
+            </a>
+            <a href="https://www.linkedin.com/in/alex-kern-8978301b4/" target="_blank"> 
+                <img className="social-link" src="https://kernify-seed.s3.us-west-1.amazonaws.com/linkedin_white_logo.png" alt="github link" />
+            </a>
+        </div>
+        </div>
+    </>
 )
 
 export default SideBar;
